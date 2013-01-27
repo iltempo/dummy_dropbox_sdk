de7a91d84cd137b9ab3077b07f44d6928c94ce40
Alexander Greim <alexxx@iltempo.de>
2013-01-27 21:26:22 +0100

    Fix case statement syntax

--- lib/dummy_dropbox_sdk.rb
+++ lib/dummy_dropbox_sdk.rb
@@ -14,10 +14,10 @@ KILO_SIZE = 1024.0
 # Return the file size with a readable style.
 def readable_file_size(size, precision)
   case
-  when size == 1 : "1 Byte"
-  when size < KILO_SIZE : "%d Bytes" % size
-  when size < MEGA_SIZE : "%.#{precision}f KB" % (size / KILO_SIZE)
-  when size < GIGA_SIZE : "%.#{precision}f MB" % (size / MEGA_SIZE)
+  when size == 1 then "1 Byte"
+  when size < KILO_SIZE then "%d Bytes" % size
+  when size < MEGA_SIZE then "%.#{precision}f KB" % (size / KILO_SIZE)
+  when size < GIGA_SIZE then "%.#{precision}f MB" % (size / MEGA_SIZE)
   else "%.#{precision}f GB" % (size / GIGA_SIZE)
   end
 end
